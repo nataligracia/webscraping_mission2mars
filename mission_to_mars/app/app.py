1939e342b7c224aa86c3e79ab5e4a07ca8bbbc22
Natali Gracia <ngracia356@gmail.com>
2021-05-13 23:21:32 -0700

    Scrape jupyter notebook

--- mission_to_mars/app/app.py
+++ mission_to_mars/app/app.py
@@ -1,16 +1,32 @@
-# import Flask, pymongo, and scrape_mars (your python file)
+#Flask, pymongo, and python file scrape_mars
 
-# Instantiate a Flask app
-
-
-# Use flask_pymongo to set up mongo connection
-
-# Create a base '/' route that will query your mongodb database and render the `index.html` template
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scrape_mars
 
 
-# Create a '/scrape' route that will create the mars collection, run your scrape() function from scrape_mars, and update the mars collection in the database
-# The route should redirect back to the base route '/' with a code 302.
+app = Flask(__name__)
 
 
+#Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
-# Run your app
+
+#302?
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape()
+    mars.replace_one({}, mars_data, upsert=True)
+    return "Scraping Successful!"
+
+
+if __name__ == "__main__":
+    app.run()
--- mission_to_mars/app/scrape_mars.py
+++ mission_to_mars/app/scrape_mars.py
@@ -1,5 +1,126 @@
-# Define a function called `scrape` that will execute all of your scraping code from the `mission_to_mars.ipynb` notebook and return one Python dictionary containing all of the scraped data. 
+#Use Jupyter Notebook to scrap information
 
-# It will be a good idea to create multiple smaller functions that are called by the `scrape()` function. 
-# Remember, each function should have one 'job' (eg. you might have a `mars_news()` function that scrapes the NASA mars news site and returns the content as a list/tuple/dictionary/json)
-# HINT: the headers in the notebook can serve as a useful guide to where one 'job' ends and another begins. 
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import datetime as dt
+import time
+import re
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def scrape():
+    
+    #Initiatie driver
+    browser = Browser("chrome", executable_path=ChromeDriverManager().install(), headless=True)
+    nasa_news_title, nasa_story_article = mars_news(browser)
+
+    #Run all scraping
+    data = {"nasa_news_title": nasa_news_title,
+            "nasa_story_article": nasa_story_article,
+            "featured_image": featured_image(browser),
+            "facts": mars_facts(),
+            "hemispheres": hemispheres(browser),
+            "last_modified": dt.datetime.now()}
+    
+    #Stop webdriver and return scraped data
+    browser.quit()
+    return data
+
+
+def mars_news(browser):
+    
+    url_news = "https://mars.nasa.gov/news/"
+    browser.visit(url_news)
+    
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=3)
+    
+    html_news = browser.html
+    soup_news = bs(html_news, "html.parser")
+    
+    try:
+        news = soup_news.select_one("ul.item_list li.slide")
+        nasa_news_title = news.find('div', class_="content_title").get_text()
+        nasa_story_article = news.find('div',class_="article_teaser_body").get_text()
+    
+    except AttributeError:
+        return None, None
+    
+    
+    return nasa_news_title, nasa_story_article
+   
+    
+def featured_image(browser):
+    
+    url_img = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
+    browser.visit(url_img)
+    
+    find_image = browser.find_by_css('a[class="showimg fancybox-thumbs"]', wait_time=3)
+    html_img= find_image[0]['href']
+    
+    featured_image_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/image/featured/mars3.jpg'
+    
+    return featured_image_url
+
+
+def mars_facts():
+    
+    df = pd.read_html("https://space-facts.com/mars/")[0]
+        
+    df.columns=['Description','Value']
+    df.set_index('Description',inplace=True)
+    
+    return df.to_html()
+
+
+def hemispheres(browser):
+    
+    url_hem = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url_hem)
+    
+    #Loop for hemispheres
+    hem_urls = []
+
+    links = browser.find_by_css("a.product-item h3")
+
+    for title in range(len(links)):
+        hemisphere ={}
+
+        browser.find_by_css("a.product-item h3")[title].click()
+
+        hem_link = browser.find_link_by_text('Sample').first
+
+        hemisphere['title'] = browser.find_by_css("h2.title").text
+
+        hemisphere['img_url'] = hem_link['href']
+
+        hem_urls.append(hemisphere)
+
+        #Navigate backwards
+        browser.back()
+    
+    return hem_urls
+
+
+if __name__ == "__main__":
+    
+    print(scrape())
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
